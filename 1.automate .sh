ef6458da3c21a15ba1c54331de1b1d8e9adb8e4e
Janardhan <janardhancloud312@gmailcom>
2026-04-01 21:20:59 +0530

    atutomate

--- 1.automate .sh
+++ 1.automate .sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Error:: kindly run with the root user"
+    exit 1
 fi
 
 dnf install mysql -y
@@ -11,6 +12,7 @@ dnf install mysql -y
 if [ $? -ne 0 ]; then
 
     echo "Installing:: Mysql is error"
+    exit 1
 else
     echo "Installing:: Mysql is sucess"
 fi
