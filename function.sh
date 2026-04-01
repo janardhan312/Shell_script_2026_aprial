9eaf50a1dcea0fc3869dfb5c4bd3e04feb45df69
Janardhan <janardhancloud312@gmailcom>
2026-04-01 21:36:54 +0530

    atutomate

---
+++ function.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Error:: kindly run with the root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+
+        echo "Installing:: $2 is error"
+        exit 1
+    else
+        echo "Installing:: $2 is sucess"
+    fi
+
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
+
+   
